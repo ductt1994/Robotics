c1d8c601be2dcad6b3f6b2ba7c1366585d64a01d
Thanh Tran <ductt1994@gmail.com>
2020-11-17 21:19:00 +0100

    Exercise 1 finished

--- Übungen/Sheet_01/sheet01.py
+++ Übungen/Sheet_01/sheet01.py
@@ -1,12 +1,28 @@
 #Import libs
 import numpy as np
+import matplotlib.pyplot as plot
 
-a = np.array([[1,1],
-              [1,1]])
+#Robot senses object at position (-2,2)
+#Vehicle coordinate system
 
-b = np.array([[2,0],
-              [0,2]])
+#Transform to global coordinate system
+#Global to Robot
+#Rotation by +90 °
+#Translation by [0,1] in global coordinate system
 
-print(a.dot(b))
+#Matrix product of Translation -> Rotation
 
-print(a * b)
+#T(trans) x T(rot):
+delta_x = 0
+delta_y = 1
+theta = np.deg2rad(90)
+
+T = np.array([[np.cos(theta),  -np.sin(theta),    delta_x],
+              [np.sin(theta),   np.cos(theta),    delta_y],
+              [0,               0,                1      ]])
+
+p1 = np.array([[2,-2,1]]).transpose()
+
+p2 = T.dot(p1)
+
+print(p2)
